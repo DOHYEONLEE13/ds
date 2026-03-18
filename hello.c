5095a1452017038d75a5032c69c80a178d0d4c58
Dohyeon Lee <dohyeonlee@cu.ac.kr>
2026-03-18 15:03:15 +0900

    add hello.c

---
+++ hello.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void main() {
+	char c, c_array[100];
+	int i, i_array[100];
+	short s, s_array[100];
+	float f, f_array[100];
+	long l, l_array[100];
+
+	printf("\n char c 크기 = %d /t: char c_array 크기 = %4d",sizeof(c), sizeof(c_array));
+
+	getchar(); 
+}
